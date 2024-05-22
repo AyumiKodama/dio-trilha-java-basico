bacb06746f1ac235a459864fd7c151c9edb825cf
Isabella Kodama <isabellakodama@gmail.com>
2024-05-22 12:43:38 -0300

    tipos-variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte a; // para números de -128 e o valor máximo é de 127. 
+        short b; // para valores de -32.768 e o valor máximo de 32.767
+        int c; // para valores de -2.147.483.648 e o valor máximo de 2.147.483.647. 
+        long d; // para valores de  de -9.223.372.036.854.775.808 e o valor máximo de 9.223.372.036.854.775.807. Ao atribuir um valor a variáveis do tipo long deve-se adicionar um L ao final.
+
+        float e; // de precisão simples
+        double f; // precisão dupla (geralmente escolha padrão)
+
+        char g; // para armazenar um caractere
+        boolean h; // armazena apenas valores true/false
+
     }
 }
